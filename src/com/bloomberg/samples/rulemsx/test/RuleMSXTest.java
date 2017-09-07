e9b78fc9a0b8dc37d58f4e2cd10d37ef86a4d938
Rik Clegg <rik.clegg@gmail.com>
2017-09-07 16:10:39 +0100

    Using new interfaces for evaluators and actions

--- src/com/bloomberg/samples/rulemsx/test/RuleMSXTest.java
+++ src/com/bloomberg/samples/rulemsx/test/RuleMSXTest.java
@@ -1,5 +1,8 @@
 package com.bloomberg.samples.rulemsx.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import com.bloomberg.emsx.samples.EasyMSX;
 import com.bloomberg.emsx.samples.EasyMSX.Environment;
 import com.bloomberg.emsx.samples.Field;
@@ -8,29 +11,24 @@ import com.bloomberg.emsx.samples.Log;
 import com.bloomberg.emsx.samples.Notification;
 import com.bloomberg.emsx.samples.NotificationHandler;
 import com.bloomberg.emsx.samples.Order;
+import com.bloomberg.mktdata.samples.EasyMKT;
+import com.bloomberg.mktdata.samples.Notification.NotificationType;
+import com.bloomberg.mktdata.samples.Security;
 import com.bloomberg.samples.rulemsx.DataPoint;
 import com.bloomberg.samples.rulemsx.DataPointSource;
 import com.bloomberg.samples.rulemsx.DataSet;
+import com.bloomberg.samples.rulemsx.Rule;
+import com.bloomberg.samples.rulemsx.RuleAction;
+import com.bloomberg.samples.rulemsx.RuleEvaluator;
 import com.bloomberg.samples.rulemsx.RuleMSX;
 import com.bloomberg.samples.rulemsx.RuleMSX.DataPointState;
-import com.bloomberglp.blpapi.CorrelationID;
-import com.bloomberglp.blpapi.Event;
-import com.bloomberglp.blpapi.EventHandler;
-import com.bloomberglp.blpapi.Message;
-import com.bloomberglp.blpapi.MessageIterator;
-import com.bloomberglp.blpapi.Request;
-import com.bloomberglp.blpapi.Service;
-import com.bloomberglp.blpapi.Session;
-import com.bloomberglp.blpapi.SessionOptions;
+import com.bloomberg.samples.rulemsx.RuleSet;
 
 public class RuleMSXTest {
 
 	private RuleMSX rmsx;
 	private EasyMSX emsx;
-	
-	private Session session;
-	
-	private boolean sessionReady=false;
+	private EasyMKT emkt;
 	
     public static void main(String[] args) throws java.lang.Exception
     {
@@ -62,32 +60,27 @@ public class RuleMSXTest {
  	
     	System.out.println("...done.");
     	
-    	System.out.println("Creating session...");
+    	System.out.println("Instantiating EasyMKT...");
+    	this.emkt = new EasyMKT();
+    	System.out.println("...done.");
 
-    	SessionOptions d_sessionOptions = new SessionOptions();
-        d_sessionOptions.setServerHost("localhost");
-        d_sessionOptions.setServerPort(8194);
-
-        Session session = new Session(d_sessionOptions, new EMSXEventHandler());
-        
-        try {
-			session.startAsync();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-        while(!sessionReady);
     }
 
     public void run() {
 
     	System.out.println("RuleMSXTest running...");
     	
-    	// Iterate through all the orders in the Order blotter
+		emkt.addField("BID");
+		emkt.addField("ASK");
+		emkt.addField("MID");
+		emkt.addField("LAST_PRICE");
+
+		DataSet rmsxTest;
+		
     	for(Order o: emsx.orders) {
 
     		// Create new DataSet for each order
-    		DataSet rmsxTest = this.rmsx.createDataSet("RMSXTest" + o.field("EMSX_SEQUENCE").value());
+    		rmsxTest = this.rmsx.createDataSet("RMSXTest" + o.field("EMSX_SEQUENCE").value());
  
     		System.out.println("New DataSet created: " + rmsxTest.getName());
  
@@ -104,12 +97,22 @@ public class RuleMSXTest {
         	amount.setDataPointSource(new EMSXFieldDataPoint(o.field("EMSX_AMOUNT"),amount));
         	System.out.println("New DataPoint added : " + amount.getName());
 
+        	DataPoint exchange = rmsxTest.addDataPoint("Exchange");
+        	exchange.setDataPointSource(new EMSXFieldDataPoint(o.field("EMSX_EXCHANGE"),exchange));
+        	System.out.println("New DataPoint added : " + exchange.getName());
+
         	DataPoint isin = rmsxTest.addDataPoint("ISIN");
         	isin.setDataPointSource(new RefDataDataPoint("ID_ISIN",o.field("EMSX_TICKER").value()));
         	System.out.println("New DataPoint added : " + isin.getName());
 
+        	System.out.println("Adding order secuity to EasyMKT...");
+        	Security sec = emkt.securities.get(o.field("EMSX_TICKER").value());
+        	if(sec==null) {
+        		sec = emkt.addSecurity(o.field("EMSX_TICKER").value());
+        	}
+        	
         	DataPoint lastPrice = rmsxTest.addDataPoint("LastPrice");
-        	lastPrice.setDataPointSource(new MktDataDataPoint("LAST_PRICE",o.field("EMSX_TICKER").value()));
+        	lastPrice.setDataPointSource(new MktDataDataPoint("LAST_PRICE",sec));
         	System.out.println("New DataPoint added : " + lastPrice.getName());
         	
         	DataPoint margin = rmsxTest.addDataPoint("Margin");
@@ -121,9 +124,26 @@ public class RuleMSXTest {
         	price.addDependency(margin);
         	price.addDependency(lastPrice);
         	System.out.println("New DataPoint added : " + price.getName());
-        	
     	}
+
+    	System.out.println("Starting EasyMKT...");
+    	emkt.start();
+    	System.out.println("EasyMKT started.");
     	
+    	// Create new RuleSet
+    	RuleSet rset = rmsx.createRuleSet("TestRules");
+    	
+    	Rule ruleIsLNExchange = new Rule("IsLNExchange",new StringEqualityRule("Exchange","UK"), new RouteToBroker("BB"));
+    	Rule ruleIsUSExchange = new Rule("IsUSExchange",new StringEqualityRule("Exchange","US"), new RouteToBroker("DMTB"));
+    	Rule ruleIsIBM = new Rule("IsIBM",new StringEqualityRule("Ticker","IBM US Equity"), new SendAdditionalSignal("This is IBM!!"));
+    	
+    	rset.addRule(ruleIsLNExchange); // Parent is ruleset, so considered an Alpha node
+    	rset.addRule(ruleIsUSExchange); // Parent is ruleset, so considered an Alpha node
+    	ruleIsUSExchange.addRule(ruleIsIBM); // Parent is another rule, so considered a Beta node
+
+    	for(DataSet ds: rmsx.getDataSets()) {
+    		
+    	}
     }
 
     class EMSXFieldDataPoint implements DataPointSource,NotificationHandler {
@@ -206,23 +226,21 @@ public class RuleMSXTest {
     	
     }
     
-    class MktDataDataPoint implements DataPointSource {
+    class MktDataDataPoint implements DataPointSource, com.bloomberg.mktdata.samples.NotificationHandler {
 
     	private String fieldName;
-    	private String ticker;
+    	private Security security;
     	private String value;
     	private boolean isStale=true;
     	
-    	MktDataDataPoint(String fieldName, String ticker) {
-    	
+    	MktDataDataPoint(String fieldName, Security security) {
     		this.fieldName = fieldName;
-    		this.ticker = ticker;
-    		//InitialiseSubscription();
+    		this.security = security;
+    		this.security.field(this.fieldName).addNotificationHandler(this);
     	}
     	
 		@Override
 		public Object getValue() {
-			// Value is always 'CURRENT', as it is maintained by the subscription
 			return value;
 		}
 
@@ -235,6 +253,15 @@ public class RuleMSXTest {
 		@Override
 		public void setState(DataPointState state) {
 			this.isStale = (state==DataPointState.STALE);
+		}
+
+		@Override
+		public void processNotification(com.bloomberg.mktdata.samples.Notification notification) {
+			if(notification.type==NotificationType.FIELD) {
+				this.value = notification.getFieldChanges().get(0).newValue;
+				System.out.println("Update for " + this.security.getName() + ": " + this.fieldName + "=" + this.value);
+			}
+			this.setState(DataPointState.STALE);
 		}
     	
     }
@@ -299,181 +326,55 @@ public class RuleMSXTest {
 		}
     }
     
-    class EMSXEventHandler implements EventHandler
-    {
-        public void processEvent(Event event, Session session)
-        {
-            try {
-                switch (event.eventType().intValue())
-                {                
-                case Event.EventType.Constants.SESSION_STATUS:
-                    processSessionEvent(event, session);
-                    break;
-                case Event.EventType.Constants.SERVICE_STATUS:
-                    processServiceEvent(event, session);
-                    break;
-                case Event.EventType.Constants.RESPONSE:
-                    processResponseEvent(event, session);
-                    break;
-                case Event.EventType.Constants.SUBSCRIPTION_STATUS:
-                    processResponseEvent(event, session);
-                    break;
-                case Event.EventType.Constants.SUBSCRIPTION_DATA:
-                    processResponseEvent(event, session);
-                    break;
-                default:
-                    processMiscEvents(event, session);
-                    break;
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-		private boolean processSessionEvent(Event event, Session session) throws Exception {
-
-			System.out.println("Processing " + event.eventType().toString());
-        	
-			MessageIterator msgIter = event.messageIterator();
-            
-			while (msgIter.hasNext()) {
-            
-				Message msg = msgIter.next();
-                
-				if(msg.messageType().equals(SESSION_STARTED)) {
-                	System.out.println("Session started...");
-                	session.openServiceAsync(d_service);
-                } else if(msg.messageType().equals(SESSION_STARTUP_FAILURE)) {
-                	System.err.println("Error: Session startup failed");
-                	return false;
-                }
-            }
-            return true;
+    class StringEqualityRule implements RuleEvaluator {
+    	
+    	String dataPointName;
+    	String match;
+    	
+    	public StringEqualityRule(String dataPointName, String match ) {
+    		
+    		this.dataPointName = dataPointName;
+    		this.match = match;
 		}
 
-        private boolean processServiceEvent(Event event, Session session) {
-
-        	System.out.println("Processing " + event.eventType().toString());
-        	
-        	MessageIterator msgIter = event.messageIterator();
-            
-        	while (msgIter.hasNext()) {
-            
-        		Message msg = msgIter.next();
-                
-        		if(msg.messageType().equals(SERVICE_OPENED)) {
-                
-        			System.out.println("Service opened...");
-                	
-                    Service service = session.getService(d_service);
-
-                    Request request = service.createRequest("CreateOrder");
-
-            	    // The fields below are mandatory
-            	    request.set("EMSX_TICKER", "IBM US Equity");
-            	    request.set("EMSX_AMOUNT", 1000);
-            	    request.set("EMSX_ORDER_TYPE", "MKT");
-            	    request.set("EMSX_TIF", "DAY");
-            	    request.set("EMSX_HAND_INSTRUCTION", "ANY");
-            	    request.set("EMSX_SIDE", "BUY");
-            	
-            	    // The fields below are optional
-            	    request.set("EMSX_ACCOUNT","TestAccount");
-            	    //request.set("EMSX_BASKET_NAME", "HedgingBasket");
-            	    request.set("EMSX_BROKER", "BMTB");
-            	    //request.set("EMSX_CFD_FLAG", "1");
-            	    //request.set("EMSX_CLEARING_ACCOUNT", "ClrAccName");
-            	    //request.set("EMSX_CLEARING_FIRM", "FirmName");
-            	    //request.set("EMSX_CUSTOM_NOTE1", "Note1");
-            	    //request.set("EMSX_CUSTOM_NOTE2", "Note2");
-            	    //request.set("EMSX_CUSTOM_NOTE3", "Note3");
-            	    //request.set("EMSX_CUSTOM_NOTE4", "Note4");
-            	    //request.set("EMSX_CUSTOM_NOTE5", "Note5");
-            	    //request.set("EMSX_EXCHANGE_DESTINATION", "ExchDest");
-            	    //request.set("EMSX_EXEC_INSTRUCTIONS", "AnyInst");
-            	    //request.set("EMSX_GET_WARNINGS", "0");
-            	    //request.set("EMSX_GTD_DATE", "20170105");
-            	    //request.set("EMSX_INVESTOR_ID", "InvID");
-            	    //request.set("EMSX_LIMIT_PRICE", 123.45);
-            	    //request.set("EMSX_LOCATE_BROKER", "BMTB");
-            	    //request.set("EMSX_LOCATE_ID", "SomeID");
-            	    //request.set("EMSX_LOCATE_REQ", "Y");
-            	    //request.set("EMSX_NOTES", "Some notes");
-            	    //request.set("EMSX_ODD_LOT", "0");
-            	    //request.set("EMSX_ORDER_ORIGIN", "");
-            	    //request.set("EMSX_ORDER_REF_ID", "UniqueID");
-            	    //request.set("EMSX_P_A", "P");
-            	    //request.set("EMSX_RELEASE_TIME", 34341);
-            	    //request.set("EMSX_REQUEST_SEQ", 1001);
-            	    //request.set("EMSX_SETTLE_CURRENCY", "USD");
-            	    //request.set("EMSX_SETTLE_DATE", 20170106);
-            	    //request.set("EMSX_SETTLE_TYPE", "T+2");
-            	    //request.set("EMSX_STOP_PRICE", 123.5);
-
-            	    System.out.println("Request: " + request.toString());
-
-                    requestID = new CorrelationID();
-                    
-                    // Submit the request
-                	try {
-                        session.sendRequest(request, requestID);
-                	} catch (Exception ex) {
-                		System.err.println("Failed to send the request");
-                		return false;
-                	}
-                	
-                } else if(msg.messageType().equals(SERVICE_OPEN_FAILURE)) {
-                	System.err.println("Error: Service failed to open");
-                	return false;
-                }
-            }
-            return true;
+		@Override
+		public boolean evaluate(DataSet dataSet) {
+			return dataSet.getDataPoint(this.dataPointName).getSource().getValue().equals(this.match);
 		}
 
-		private boolean processResponseEvent(Event event, Session session) throws Exception 
-		{
-			System.out.println("Received Event: " + event.eventType().toString());
-            
-            MessageIterator msgIter = event.messageIterator();
-            
-            while(msgIter.hasNext())
-            {
-            	Message msg = msgIter.next();
-        
-                System.out.println("MESSAGE: " + msg.toString());
-                System.out.println("CORRELATION ID: " + msg.correlationID());
-                
-                if(event.eventType()==Event.EventType.RESPONSE && msg.correlationID()==requestID) {
-                	
-                	System.out.println("Message Type: " + msg.messageType());
-                	if(msg.messageType().equals(ERROR_INFO)) {
-                		Integer errorCode = msg.getElementAsInt32("ERROR_CODE");
-                		String errorMessage = msg.getElementAsString("ERROR_MESSAGE");
-                		System.out.println("ERROR CODE: " + errorCode + "\tERROR MESSAGE: " + errorMessage);
-                	} else if(msg.messageType().equals(CREATE_ORDER)) {
-                		Integer emsx_sequence = msg.getElementAsInt32("EMSX_SEQUENCE");
-                		String message = msg.getElementAsString("MESSAGE");
-                		System.out.println("EMSX_SEQUENCE: " + emsx_sequence + "\tMESSAGE: " + message);
-                	}
-                	                	
-                	quit=true;
-                	session.stop();
-                }
-            }
-            return true;
+		@Override
+		public ArrayList<String> getDependencies() {
+			return new ArrayList<String>(Arrays.asList(this.dataPointName));
 		}
-		
-        private boolean processMiscEvents(Event event, Session session) throws Exception 
-        {
-            System.out.println("Processing " + event.eventType().toString());
-            MessageIterator msgIter = event.messageIterator();
-            while (msgIter.hasNext()) {
-                Message msg = msgIter.next();
-                System.out.println("MESSAGE: " + msg);
-            }
-            return true;
-        }
+    }
+    
+    class RouteToBroker implements RuleAction {
 
-    }	
+    	private String brokerCode;
+    	
+    	public RouteToBroker(String brokerCode) {
+    		this.brokerCode = brokerCode;
+    	}
+    	
+    	@Override
+		public void execute(DataSet dataSet) {
+    		// create route instruction for the order based only on data from the dataset
+    		System.out.println("Created route to broker '" + this.brokerCode + "' for DataSet: " + dataSet.getName());
+		}
+    }
+    
+    class SendAdditionalSignal implements RuleAction {
 
+    	private String signal;
+    	
+    	public SendAdditionalSignal(String signal) {
+    		this.signal = signal;
+    	}
+    	
+		@Override
+		public void execute(DataSet dataSet) {
+			System.out.println("Sending Signal for " + dataSet.getName() + ": " + this.signal);
+		}
+    	
+    }
 }
